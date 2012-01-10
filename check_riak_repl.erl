9cf18c632baf2fbc1c594957a0f838116ae120f2
Joe DeVivo <joe.devivo@basho.com>
2012-01-10 14:54:48 -0700

    riak_repl check working

--- check_riak_repl.erl
+++ check_riak_repl.erl
@@ -2,12 +2,12 @@
 
 value(null) -> null;
 value(String) ->
-    io:format("~s~n", [String]),
-    S = string:concat(re:replace(String, "<[^>]*>", "\"&\"", [global, {return, list}]), "."),
-    io:format("~s~n", [S]),
+    %io:format("~s~n", [String]),
+    S = string:concat(re:replace(String, "<[^>]*>", "{pid, \"&\"}", [global, {return, list}]), "."),
+    %io:format("~s~n", [S]),
     {ok,Tokens,_} = erl_scan:string(S),
     {ok,Term} = erl_parse:parse_term(Tokens),
-    io:format("~p~n", [Term]),
+    %io:format("~p~n", [Term]),
     Term.
 
 %% How to handle the "Attempting to restart script through sudo -u riak" line?
@@ -26,9 +26,47 @@ object_acc([H|T], {Name, Value}, Objects) ->
 object_acc([], {Name, Value}, Objects) ->
     [{Name, value(Value)}|Objects].
     
-main(_) ->
+main([Type, Site]) ->
+    %Args = string:token(Arg, " "),
     Status = objects(string:tokens(os:cmd("riak-repl status"), "\n")),
-    io:format("~p~n", [Status]).
+    %Stats = find_stats(lists:nth(1, Args), lists:nth(2, Args), Status),
+    Stats = find_stats(Type, Site, Status),
+    {state, ConnectionState} = Stats,
+    case ConnectionState of
+        % client
+        disconnected    -> nagios:critical("Disconnected"); 
+        connecting      -> nagios:critical("Connecting");
+        wait_peerinfo   -> nagios:okay("Wait PeerInfo");
+        merkle_recv     -> nagios:okay("Merkle Recv");
+        merkle_diff     -> nagios:okay("Merkle Diff");
+        merkle_exchange -> nagios:okay("Merkle Exchange");
+        
+        %server
+        send_peerinfo   -> nagios:okay("send_peerinfo");
+        wait_peerinfo   -> nagios:okay("wait_peerinfo");
+        merkle_send     -> nagios:okay("merkle_send");
+        merkle_build    -> nagios:okay("merkle_build");
+        merkle_xfer     -> nagios:okay("merkle_xfer");
+        merkle_wait_ack -> nagios:okay("merkle_wait_ack");
+        %merkle_diff     -> nagios:okay("merkle_diff");
+        connected       -> nagios:okay("connected");
+        server_not_found -> nagios:critical("Server Not Found");
+        Response -> nagios:unknown(string:concat("I don't know what to make of ", Response))
+    end.
     
     %% each line that has a (:), parse value following the colon, until the next line with a colon, or end.
     
+find_stats("client", Site, Status) ->
+    find_stats("client_stats", Site, Status);
+find_stats("server", Site, Status) ->
+    find_stats("server_stats", Site, Status);
+find_stats(StatKey, Site, Status) ->
+    {StatKey, Stats} = lists:keyfind(StatKey, 1, Status),
+    ListOfStats = [X || {{pid, _}, {message_queue_len, _}, {status, X}} <- Stats],
+    Stat = [lists:keyfind(state, 1, S) || S <- ListOfStats, lists:keyfind(Site, 2, S) =/= false],
+    case Stat of
+        [] -> {state, server_not_found};
+        _ -> lists:nth(1, [lists:keyfind(state, 1, S) || S <- ListOfStats, lists:keyfind(Site, 2, S) =/= false])
+    end.
+% TODO: If server stats has no key for what you're looking for, it's down, otherwise it's up
+% I think this script doesn't handle that case.
