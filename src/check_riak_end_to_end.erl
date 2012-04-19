8e1a7f05fa282b746fd74b79ecc23d8b08efff7a
Joe DeVivo <joe.devivo@basho.com>
2012-04-19 08:46:13 -0700

    More robust test for end_to_end

--- src/check_riak_end_to_end.erl
+++ src/check_riak_end_to_end.erl
@@ -10,63 +10,64 @@
 % 3) HTTP DELETE
 % 4) Confirm DELETE via HTTP GET
 
-%The _code functions below break down the output from the commands and isolates the HTTP return value (i.e. 200,204,404),
-%  if there is no return value (occurs when there's a failure to connect) then they return the value 0.
-return_code(StringList) ->
-    http_code(StringList, null).
-    
-http_code([H|T], Code) ->
-    case string:str(H, "< HTTP") of
-        0 -> http_code(T, Code);
-        _ -> http_code(T, string:sub_string(H, 12, 14))
-    end;
-http_code([], null) ->
-    "0";
-http_code([], Code) ->
-    Code.
 
-%This function takes the path that's being tested, and creates the three commands that need to be called for testing:
-%  C1 = write, C2 = read, and C3 = delete
-get_commands(Path) ->
-    C1 = string:concat(string:concat("curl -v -X PUT -d \'Hello World\' -H \"content-type: text/plain\" ", Path), "/tb/tk"),
-    C2 = string:concat(string:concat("curl -v ", Path), "/tb/tk"),
-    C3 = string:concat(string:concat("curl -v -X DELETE ", Path), "/tb/tk"),
-    {C1, C2, C3}.
-    
 main([Host, Port, Path]) ->
     %Concatenate the Path to be tested
-    TestPath = string:concat(string:concat(string:concat(string:concat(string:concat("http://", Host), ":"), Port), "/"), Path),
+    TestPath = "http://" ++ Host ++ ":" ++ Port ++ "/" ++ Path ++ "/tb/tk",
     % io:put_chars(string:concat(string:concat("Testing: ", TestPath), "\n")),
 
-    %Get the three commands need to be run for writing, reading, and deleting
-    {Cmd1, Cmd2, Cmd3} = get_commands(TestPath),
+    %Create the two requests that need to be called for testing:
+    %  R1 = write, R2 = read and delete
+    R1 = {TestPath, [], "text/plain", "Hello World"},
+    R2 = {TestPath, []},
 
     %Test writing, reading, deleting, and reading (to confirm delete)
-    {Stat1, _} = string:to_integer(return_code(string:tokens(os:cmd(Cmd1), "\n"))),
-    if 
-        Stat1 =/= 204 -> riak_nagios:critical("Write Failed"); 
-        true -> true
+    inets:start(),
+
+    case test(put, R1, 204) of
+        ok -> ok;
+        _  -> riak_nagios:critical("Write Failed")
     end,
     
-    {Stat2, _} = string:to_integer(return_code(string:tokens(os:cmd(Cmd2), "\n"))),
-    if  
-        Stat2 =/= 200 -> riak_nagios:critical("First Read Failed");
-        true -> true
+    timer:sleep(500),
+
+    case test(get, R2, 200) of
+        ok -> ok;
+        _  -> riak_nagios:critical("First Read Failed")
     end,
     
-    {Stat3, _} = string:to_integer(return_code(string:tokens(os:cmd(Cmd3), "\n"))),
-    if
-        Stat3 =/= 204 -> riak_nagios:critical("Delete Failed");
-        true -> true
+    timer:sleep(500),
+
+    case test(delete, R2, 204) of
+        ok -> ok;
+        _  -> riak_nagios:critical("Delete Failed")
     end,
     
-    {Stat4, _} = string:to_integer(return_code(string:tokens(os:cmd(Cmd2), "\n"))),
-    if
-        Stat4 =/= 404 -> riak_nagios:critical("Second Read Failed");
-        true -> riak_nagios:okay("Test Passed")
-    end;
+    timer:sleep(500),
+
+    case test(get, R2, 404) of
+        ok -> riak_nagios:okay("Test Passed");
+        _  -> riak_nagios:critical("Second Read Failed")
+    end,
+
+    inets:stop();
+
 main(_) ->
     usage().
+
+test(M,R,S) -> test(M,R,S,5).
+test(M,R,S,N) -> 
+    case httpc:request(M, R, [], []) of
+        {ok,{{_,S,_},_,_}} -> ok;
+        _ ->
+            case N of
+                5 -> timer:sleep(500) , test(M,R,S,N-1);
+                4 -> timer:sleep(1000), test(M,R,S,N-1);
+                3 -> timer:sleep(2000), test(M,R,S,N-1);
+                2 -> timer:sleep(4000), test(M,R,S,N-1);
+                _ -> fail
+            end
+    end.
 
 usage() ->
     io:format("Usage: check_end_to_end.erl hostname port path~n"),
