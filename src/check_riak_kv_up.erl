a5776023a6c007316e6376fd0dca903770034133
Joe Caswell <jcaswell@basho.com>
2012-10-05 12:34:04 -0400

    Check that rpc returned a list before calling lists:member

--- src/check_riak_kv_up.erl
+++ src/check_riak_kv_up.erl
@@ -8,9 +8,14 @@ run(Options, _) ->
 
 check_riak_kv_up(Node) ->
     Services = rpc:call(Node, riak_core_node_watcher, services, []),
+    check_riak_kv_services(Node, Services).
+
+check_riak_kv_services(Node, Services) when is_list(Services) ->
     case lists:member(riak_kv, Services) of
         true ->
             {ok, "riak_kv is running on ~s", [Node]};
         false ->
             {critical, "riak_kv not found in running services on ~s: ~p", [Node, Services]}
-    end.        
+    end;
+check_riak_kv_services(Node, Services) ->
+    {critical, "Unable to get list of services running on ~s: ~p", [Node, Services]}.
